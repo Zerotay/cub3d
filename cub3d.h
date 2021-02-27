dddc0350bec39b3033b82d2ba90050b0e1c95024
Donggun Kim <dongguki.42seoul.kr>
2021-02-27 17:21:52 +0900

    how

--- cub3d.h
+++ cub3d.h
@@ -23,30 +23,30 @@
 # define NEXT (char *)tmp->next->content
 
 #define NoEventMask					0L
-#define KeyPressMask				(1L<<0)  
-#define KeyReleaseMask				(1L<<1)  
-#define ButtonPressMask				(1L<<2)  
-#define ButtonReleaseMask			(1L<<3)  
-#define EnterWindowMask				(1L<<4)  
-#define LeaveWindowMask				(1L<<5)  
-#define PointerMotionMask			(1L<<6)  
-#define PointerMotionHintMask		(1L<<7)  
-#define Button1MotionMask			(1L<<8)  
-#define Button2MotionMask			(1L<<9)  
-#define Button3MotionMask			(1L<<10) 
-#define Button4MotionMask			(1L<<11) 
-#define Button5MotionMask			(1L<<12) 
-#define ButtonMotionMask			(1L<<13) 
+#define KeyPressMask				(1L<<0)
+#define KeyReleaseMask				(1L<<1)
+#define ButtonPressMask				(1L<<2)
+#define ButtonReleaseMask			(1L<<3)
+#define EnterWindowMask				(1L<<4)
+#define LeaveWindowMask				(1L<<5)
+#define PointerMotionMask			(1L<<6)
+#define PointerMotionHintMask		(1L<<7)
+#define Button1MotionMask			(1L<<8)
+#define Button2MotionMask			(1L<<9)
+#define Button3MotionMask			(1L<<10)
+#define Button4MotionMask			(1L<<11)
+#define Button5MotionMask			(1L<<12)
+#define ButtonMotionMask			(1L<<13)
 #define KeymapStateMask				(1L<<14)
-#define ExposureMask				(1L<<15) 
-#define VisibilityChangeMask		(1L<<16) 
-#define StructureNotifyMask			(1L<<17) 
-#define ResizeRedirectMask			(1L<<18) 
-#define SubstructureNotifyMask		(1L<<19) 
-#define SubstructureRedirectMask	(1L<<20) 
-#define FocusChangeMask				(1L<<21) 
-#define PropertyChangeMask			(1L<<22) 
-#define ColormapChangeMask			(1L<<23) 
+#define ExposureMask				(1L<<15)
+#define VisibilityChangeMask		(1L<<16)
+#define StructureNotifyMask			(1L<<17)
+#define ResizeRedirectMask			(1L<<18)
+#define SubstructureNotifyMask		(1L<<19)
+#define SubstructureRedirectMask	(1L<<20)
+#define FocusChangeMask				(1L<<21)
+#define PropertyChangeMask			(1L<<22)
+#define ColormapChangeMask			(1L<<23)
 #define OwnerGrabButtonMask			(1L<<24)
 
 #define KeyPress			2
@@ -85,14 +85,16 @@
 #define GenericEvent		35
 #define LASTEvent			36
 
+
+#define POSITION			(char *)curr->content + cub.map.position_x
 /*
-   \ C | B / 
-    \  |  /  
+   \ C | B /
+    \  |  /
  D   \ | /   A
   -----------       X
  E   / | \   H
-    /  |  \  
-   / F | G \	
+    /  |  \
+   / F | G \
 
        |
    B   |   A
--- raycasting.c
+++ raycasting.c
@@ -2,46 +2,46 @@
 
 t_cub   cub_init(t_cub cub)
 {
-    cub.mlx = 0;
-    cub.win = 0;
-    cub.img_we.img = 0;
-    cub.img_ea.img = 0;
-    cub.img_s.img = 0;
-    cub.img_no.img = 0;
-    cub.img_so.img = 0;
-    cub.real_x = 0;
-    cub.real_y = 0;
-    return (cub);
+	cub.mlx = 0;
+	cub.win = 0;
+	cub.img_we.img = 0;
+	cub.img_ea.img = 0;
+	cub.img_s.img = 0;
+	cub.img_no.img = 0;
+	cub.img_so.img = 0;
+	cub.real_x = 0;
+	cub.real_y = 0;
+	return (cub);
 }
 
 void    free_mlx(t_cub cub)
 {
-    if (cub.img_ea.img)
-        mlx_destroy_image(cub.mlx, cub.img_ea.img);
-    if (cub.img_no.img)
-        mlx_destroy_image(cub.mlx, cub.img_no.img);
-    if (cub.img_s.img)
-        mlx_destroy_image(cub.mlx, cub.img_s.img);
-    if (cub.img_so.img)
-        mlx_destroy_image(cub.mlx, cub.img_so.img);
-    if (cub.img_we.img)
-        mlx_destroy_image(cub.mlx, cub.img_we.img);
-    if (cub.win)
-        mlx_destroy_window(cub.mlx, cub.win);
+	if (cub.img_ea.img)
+		mlx_destroy_image(cub.mlx, cub.img_ea.img);
+	if (cub.img_no.img)
+		mlx_destroy_image(cub.mlx, cub.img_no.img);
+	if (cub.img_s.img)
+		mlx_destroy_image(cub.mlx, cub.img_s.img);
+	if (cub.img_so.img)
+		mlx_destroy_image(cub.mlx, cub.img_so.img);
+	if (cub.img_we.img)
+		mlx_destroy_image(cub.mlx, cub.img_we.img);
+	if (cub.win)
+		mlx_destroy_window(cub.mlx, cub.win);
 }
 
 int     exit_pro(int keycode, t_cub *cub)
 {
-    printf("You clicked %d\n", keycode);
-    if (keycode == 53)
-    {
-        free_all(cub->map);
-        free_mlx(*cub);
-        printf("bye~~\n");
-        exit(0);
-    }
+	printf("You clicked %d\n", keycode);
+	if (keycode == 53)
+	{
+		free_all(cub->map);
+		free_mlx(*cub);
+		printf("bye~~\n");
+		exit(0);
+	}
 
-        return (0);
+		return (0);
 }
 
 
@@ -81,9 +81,9 @@ int     exit_pro(int keycode, t_cub *cub)
 
 // t_cub   singleline(t_cub cub, int ray_x)
 // {
-    // double deg;
-    // char news;
-    // double dist;
+	// double deg;
+	// char news;
+	// double dist;
 
 // //실제 위치도 써먹어야 한다.
 
@@ -129,98 +129,334 @@ int     exit_pro(int keycode, t_cub *cub)
 //     return (cub);
 // }
 
-double  shoot_a(t_cub cub, double deg, char *news)
+double  shoot_d(t_cub cub, double deg, char *news)
 {
-    double  light_x;
-    double  light_y;
-    double  func;
-    double  gunc;
-    t_list  *curr;
+	double  light_x;
+	double  light_y;
+	double  func;
+	double  gunc;
+	t_list  *curr;
 
-    light_x = 1;
-    light_y = 1;
-    curr = cub.map.position_y;
-    while (1)
-    {
-        func = tan(deg) * (light_x - cub.real_x) + cub.real_y;
-        gunc = (1 / tan(deg)) * (light_y - cub.real_y) + cub.real_x;
+	light_x = 1;
+	light_y = 0;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		func = tan(deg) * (light_x - cub.real_x) + cub.real_y;
+		gunc = (1 / tan(deg)) * (light_y - cub.real_y) + cub.real_x;
 
-        if (hypot(light_x, func) < hypot(light_y, gunc))
-        {
-            if (*(char *)(curr->content + cub.map.position_x + (int)light_x) == '1')
-            {
-                *news = 'w';
-                return (hypot((light_x - cub.real_x), (func - cub.real_y))); //특정 벽 이미지를 출력하는 함수로연결해도 될 듯
-            }
-            else
-                light_x++;
-        }
-        else
-        {
-            curr = curr->prev;
-            if (*(char *)(curr->content + cub.map.position_x + (int)light_x - 1) == '1')
-            {
-                *news = 's';
-                return (hypot((gunc - cub.real_x), (light_y - cub.real_y)));
-            }
-            else
-                light_y++;
-        }
-    }
+		if (hypot(light_x, func) < hypot(light_y, gunc))
+		{
+			if (*(POSITION + (int)light_x) == '1')
+			{
+				*news = 'w';
+				return (hypot((light_x - cub.real_x), (func - cub.real_y))); //특정 벽 이미지를 출력하는 함수로 연결해도 될 듯
+			}
+			else
+				light_x++;
+		}
+		else
+		{
+			curr = curr->next;
+			if (*(POSITION + (int)light_x) == '1')
+			{
+				*news = 'n';
+				return (hypot((gunc - cub.real_x), (light_y - cub.real_y))); // deg값도 넘겨줘야 진정한 거리를 알 수 있다.
+			}
+			else
+				light_y--;
+		}
+	}
 }
 
-int singlelight(t_cub cub, int ray_x)
+double  shoot_c(t_cub cub, double deg, char *news)
 {
-    double deg;
-    char news;
-    double dist;
+	double  light_x;
+	double  light_y;
+	double  func;
+	double  gunc;
+	t_list  *curr;
 
-    if (cub.map.t_flag == 'S')
-        cub.direction = 3 * M_PI_2;
-    else if (cub.map.t_flag == 'N')
-        cub.direction = M_PI_2;
-    else if (cub.map.t_flag == 'E')
-        cub.direction = 0;
-    else
-        cub.direction = M_PI;
+	light_x = 0;
+	light_y = 0;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		func = tan(deg) * (light_x - cub.real_x) + cub.real_y;
+		gunc = (1 / tan(deg)) * (light_y - cub.real_y) + cub.real_x;
 
-    deg = (cub.direction + cub.fovh / 2) - cub.fovh / (cub.map.screen_x - 1) * ray_x;
-    if (deg >= 2 * M_PI)
-        deg -= 2 * M_PI;
+		if (hypot(light_x, func) < hypot(light_y, gunc))
+		{
+			if (*(POSITION + (int)light_x - 1) == '1')
+			{
+				*news = 'e';
+				return (hypot((light_x - cub.real_x), (func - cub.real_y))); //특정 벽 이미지를 출력하는 함수로 연결해도 될 듯
+			}
+			else
+				light_x--;
+		}
+		else
+		{
+			curr = curr->next;
+			if (*(POSITION + (int)light_x - 1) == '1')
+			{
+				*news = 'n';
+				return (hypot((gunc - cub.real_x), (light_y - cub.real_y))); // deg값도 넘겨줘야 진정한 거리를 알 수 있다.
+			}
+			else
+				light_y--;
+		}
+	}
+}
 
-    if (deg > 0 && deg < M_PI_2)
-        dist = shoot_a(cub, deg, &news);
-    if (deg > M_PI_2 && deg < M_PI)
-        dist = shoot_b(cub, deg, &news);
-    if (deg > M_PI && deg < 3 * M_PI_2)
-        dist = shoot_c(cub, deg, &news);
-    if (deg > 3 * M_PI_2 && deg < 2 * M_PI)
-        dist = shoot_d(cub, deg, &news);
+double  shoot_b(t_cub cub, double deg, char *news)
+{
+	double  light_x;
+	double  light_y;
+	double  func;
+	double  gunc;
+	t_list  *curr;
+
+	light_x = 0;
+	light_y = 1;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		func = tan(deg) * (light_x - cub.real_x) + cub.real_y;
+		gunc = (1 / tan(deg)) * (light_y - cub.real_y) + cub.real_x;
+
+		if (hypot(light_x, func) < hypot(light_y, gunc))
+		{
+			if (*(POSITION + (int)light_x - 1) == '1')
+			{
+				*news = 'e';
+				return (hypot((light_x - cub.real_x), (func - cub.real_y))); //특정 벽 이미지를 출력하는 함수로 연결해도 될 듯
+			}
+			else
+				light_x--;
+		}
+		else
+		{
+			curr = curr->prev;
+			if (*(POSITION + (int)light_x) == '1')
+			{
+				*news = 's';
+				return (hypot((gunc - cub.real_x), (light_y - cub.real_y))); // deg값도 넘겨줘야 진정한 거리를 알 수 있다.
+			} //음수 처리를 해야 하나? 어차피 제곱을 할 텐데
+			else
+				light_y++;
+		}
+	}
+}
+
+double  shoot_a(t_cub cub, double deg, char *news)
+{
+	double  light_x;
+	double  light_y;
+	double  func;
+	double  gunc;
+	t_list  *curr;
+
+	light_x = 1;
+	light_y = 1;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		func = tan(deg) * (light_x - cub.real_x) + cub.real_y;
+		gunc = (1 / tan(deg)) * (light_y - cub.real_y) + cub.real_x;
+
+		if (hypot(light_x, func) < hypot(light_y, gunc))
+		{
+			if (*(POSITION + (int)light_x) == '1')
+			{
+				*news = 'w';
+				return (hypot((light_x - cub.real_x), (func - cub.real_y))); //특정 벽 이미지를 출력하는 함수로 연결해도 될 듯
+			}
+			else
+				light_x++;
+		}
+		else
+		{
+			curr = curr->prev;
+			if (*(POSITION + (int)light_x - 1) == '1')
+			{
+				*news = 's';
+				return (hypot((gunc - cub.real_x), (light_y - cub.real_y))); // deg값도 넘겨줘야 진정한 거리를 알 수 있다.
+			}
+			else
+				light_y++;
+		}
+	}
+}
+
+double shoot_up(t_cub cub, char *news)
+{
+	double  light_y;
+	t_list  *curr;
+
+	*news = 's';
+	light_y = 1;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		curr = curr->prev;
+		if (*(POSITION) == '1')
+			return (light_y - cub.real_y);
+		else
+			light_y++;
+	}
+	return (0);
+}
+
+double shoot_left(t_cub cub, char *news)
+{
+	double  light_x;
+	t_list  *curr;
+
+	*news = 'e';
+	light_x = 0;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		if (*(POSITION + (int)light_x) == '1')
+			return (-1 * (light_x - cub.real_x));
+		else
+			light_x--;
+	}
+	return (0);
+}
+
+double shoot_down(t_cub cub, char *news)
+{
+	double  light_y;
+	t_list  *curr;
+
+	*news = 'n';
+	light_y = 0;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		curr = curr->next;
+		if (*(POSITION) == '1')
+			return (-1 * (light_y - cub.real_y));
+		else
+			light_y--;
+	}
+	return (0);
+}
+
+double shoot_left(t_cub cub, char *news)
+{
+	double  light_x;
+	t_list  *curr;
+
+	*news = 'w';
+	light_x = 1;
+	curr = cub.map.position_y;
+	while (1)
+	{
+		if (*(POSITION) == '1')
+			return (light_x - cub.real_x);
+		else
+			light_x++;
+	}
+	return (0);
+}
+
+t_cub	draw_no(t_cub cub, double len)
+{
+
+	return (cub);
+}
+
+t_cub singlelight(t_cub cub, int ray_x)
+{
+	double deg;
+	char news;
+	double dist;
+	double	len;
+
+	if (cub.map.t_flag == 'S')
+		cub.direction = 3 * M_PI_2;
+	else if (cub.map.t_flag == 'N')
+		cub.direction = M_PI_2;
+	else if (cub.map.t_flag == 'E')
+		cub.direction = 0;
+	else
+		cub.direction = M_PI;
+
+	deg = (cub.direction + cub.fovh / 2) - cub.fovh / (cub.map.screen_x - 1) * ray_x;
+	if (deg >= 2 * M_PI)
+		deg -= 2 * M_PI;
+
+	if (deg > 0 && deg < M_PI_2)
+		dist = shoot_a(cub, deg, &news);
+	if (deg > M_PI_2 && deg < M_PI)
+		dist = shoot_b(cub, deg, &news);
+	if (deg > M_PI && deg < 3 * M_PI_2)
+		dist = shoot_c(cub, deg, &news);
+	if (deg > 3 * M_PI_2 && deg < 2 * M_PI)
+		dist = shoot_d(cub, deg, &news);
+	if (deg == M_PI_2)
+		dist = shoot_up(cub, &news);
+	if (deg == M_PI)
+		dist = shoot_left(cub, &news);
+	if (deg == 3 * M_PI_2)
+		dist = shoot_down(cub, &news);
+	if (deg == 0)
+		dist = shoot_right(cub, &news);
+
+//받아온 값은 정확한 거리와 벽의 방향. 추가적으로 각도 역시 가지고 있다. 각도는 나중에 휘어지는 것 방지할 때 쓰기
+
+	len = 2 * dist * tan(cub.fovv / 2);
+	if (news == 'n')
+		cub = draw_no(cub, len);
+	if (news == 's')
+		cub = draw_so(cub, len);
+	if (news == 'e')
+		cub = draw_ea(cub, len);
+	if (news == 'w')
+		cub = draw_we(cub, len);
+	return (cub);
 }
 
 void    display(t_map map)
 {
-    t_cub cub;
+	t_cub cub;
 
-    cub.map = map;
-    cub = cub_init(cub);
-    cub.mlx = mlx_init();
-    cub.win = mlx_new_window(cub.mlx, map.screen_x, map.screen_y, "레이캐스팅");
+	cub.map = map;
+	cub = cub_init(cub);
+	cub.mlx = mlx_init();
+	cub.win = mlx_new_window(cub.mlx, map.screen_x, map.screen_y, "레이캐스팅");
 
-    cub.fovh = M_PI / 3; //60도 마찌??
-    cub.fovv = cub.fovh * cub.map.screen_y / cub.map.screen_x;
+	cub.screen.img = mlx_new_image(cub.mlx, map.screen_x, map.screen_y);
+	cub.img_no.img = mlx_xpm_file_to_image(cub.mlx, map.i_no, &cub.img_no.width, &cub.img_no.height);
+	cub.img_so.img = mlx_xpm_file_to_image(cub.mlx, map.i_so, &cub.img_so.width, &cub.img_so.height);
+	cub.img_ea.img = mlx_xpm_file_to_image(cub.mlx, map.i_ea, &cub.img_ea.width, &cub.img_ea.height);
+	cub.img_we.img = mlx_xpm_file_to_image(cub.mlx, map.i_we, &cub.img_we.width, &cub.img_we.height);
+	cub.img_s.img = mlx_xpm_file_to_image(cub.mlx, map.i_s, &cub.img_s.width, &cub.img_s.height);
+
+	cub.img_no.img_adrr = mlx_get_data_addr(cub.img_no.img, &cub.img_no.bpp, &cub.img_no.sl, &cub.img_no.en);
+	cub.img_so.img_adrr = mlx_get_data_addr(cub.img_so.img, &cub.img_so.bpp, &cub.img_so.sl, &cub.img_so.en);
+	cub.img_ea.img_adrr = mlx_get_data_addr(cub.img_ea.img, &cub.img_ea.bpp, &cub.img_ea.sl, &cub.img_ea.en);
+	cub.img_we.img_adrr = mlx_get_data_addr(cub.img_we.img, &cub.img_we.bpp, &cub.img_we.sl, &cub.img_we.en);
+	cub.img_s.img_adrr = mlx_get_data_addr(cub.img_s.img, &cub.img_s.bpp, &cub.img_s.sl, &cub.img_s.en);
+
+
+	cub.fovh = M_PI / 3; //60도 마찌??
+	cub.fovv = cub.fovh * cub.map.screen_y / cub.map.screen_x;
 
 
 
-    int ray_x = 0;
-    while (ray_x < cub.map.screen_x)
-    {
-        singlelight(cub, ray_x);
-        ray_x++;
-    }
+	int ray_x = 0;
+	while (ray_x < cub.map.screen_x)
+	{
+		cub = singlelight(cub, ray_x);
+		ray_x++;
+	}
 
-    mlx_hook(cub.win, 2, 0, exit_pro, &cub);
-    mlx_loop(cub.mlx);
+	mlx_hook(cub.win, 2, 0, exit_pro, &cub);
+	mlx_loop(cub.mlx);
 }
 
 // typedef struct s_val
@@ -268,7 +504,7 @@ void    display(t_map map)
 //     {
 //         t->tcl = 1;
 //         t->color += t->tcl;
-//     }    
+//     }
 //     else if (t->color <= 0x0000ffff)
 //     {
 //         t->tcl = 0x00000100;
@@ -283,7 +519,7 @@ void    display(t_map map)
 //     {
 //         t->color = 0;
 //     }
-		
+
 //     mlx_put_image_to_window(t->mlx, t->win, t->img, 120, 60);
 // }
 // void display(void)
@@ -304,30 +540,30 @@ void    display(t_map map)
 // }
 
 // 이미지 확대와 축소
-    // cub.img_ea.img = mlx_xpm_file_to_image(cub.mlx, cub.map.i_ea, &cub.img_ea.width, &cub.img_ea.height);
-    // cub.screen.img = mlx_new_image(cub.mlx, ga, se); //나중에는 창 크기에 맞춰서 크기를 잡게 되지 않을까.
+	// cub.img_ea.img = mlx_xpm_file_to_image(cub.mlx, cub.map.i_ea, &cub.img_ea.width, &cub.img_ea.height);
+	// cub.screen.img = mlx_new_image(cub.mlx, ga, se); //나중에는 창 크기에 맞춰서 크기를 잡게 되지 않을까.
 
-    // cub.img_ea.img_adrr = mlx_get_data_addr(cub.img_ea.img, &cub.img_ea.bpp, &cub.img_ea.sl, &cub.img_ea.en);
-    // cub.screen.img_adrr = mlx_get_data_addr(cub.screen.img, &cub.screen.bpp, &cub.screen.sl, &cub.screen.en);
+	// cub.img_ea.img_adrr = mlx_get_data_addr(cub.img_ea.img, &cub.img_ea.bpp, &cub.img_ea.sl, &cub.img_ea.en);
+	// cub.screen.img_adrr = mlx_get_data_addr(cub.screen.img, &cub.screen.bpp, &cub.screen.sl, &cub.screen.en);
 
-    // int i = -1;
-    // int j = -1;
-    // char *dst;
-    // char *srcs;
-    // int x; 
-    // int y;
-    // while (++i < ga)
-    // {
-    //     j = -1;
-    //     while (++j < se)
-    //     {
-    //         x = i * cub.img_ea.width / ga;
-    //         y = j * cub.img_ea.height / se;
-    //         printf("so xy : %d, %d\n", x, y);
-    //         dst = cub.screen.img_adrr + (j * cub.screen.sl + i * (cub.screen.bpp / 8));
-    //         srcs = cub.img_ea.img_adrr + (y * cub.img_ea.sl + x * (cub.img_ea.bpp / 8));
-    //         *(unsigned int *)dst = *(unsigned int *)srcs;
-    //     }
-    // }
-    // mlx_put_image_to_window(cub.mlx, cub.win, cub.img_ea.img, 60, 60);
-    // mlx_put_image_to_window(cub.mlx, cub.win, cub.screen.img, 120, 60);
+	// int i = -1;
+	// int j = -1;
+	// char *dst;
+	// char *srcs;
+	// int x;
+	// int y;
+	// while (++i < ga)
+	// {
+	//     j = -1;
+	//     while (++j < se)
+	//     {
+	//         x = i * cub.img_ea.width / ga;
+	//         y = j * cub.img_ea.height / se;
+	//         printf("so xy : %d, %d\n", x, y);
+	//         dst = cub.screen.img_adrr + (j * cub.screen.sl + i * (cub.screen.bpp / 8));
+	//         srcs = cub.img_ea.img_adrr + (y * cub.img_ea.sl + x * (cub.img_ea.bpp / 8));
+	//         *(unsigned int *)dst = *(unsigned int *)srcs;
+	//     }
+	// }
+	// mlx_put_image_to_window(cub.mlx, cub.win, cub.img_ea.img, 60, 60);
+	// mlx_put_image_to_window(cub.mlx, cub.win, cub.screen.img, 120, 60);
